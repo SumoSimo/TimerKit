e5b35a145eb14fc4edb87b881bba56c8b44ec3db
Simon Tsai <simon.tsai.j@gmail.com>
2016-11-14 18:41:05 -0500

    Initial Commit

---
+++ TimerKit/TimerKit.h
@@ -0,0 +1,19 @@
+//
+//  TimerKit.h
+//  TimerKit
+//
+//  Created by Simon Tsai on 11/14/16.
+//  Copyright © 2016 Simon Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimerKit.
+FOUNDATION_EXPORT double TimerKitVersionNumber;
+
+//! Project version string for TimerKit.
+FOUNDATION_EXPORT const unsigned char TimerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimerKit/PublicHeader.h>
+
+
