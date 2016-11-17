87c6115fd1179af26d21dc63463790319c01259d
Simon Tsai <simon.tsai.j@gmail.com>
2016-11-17 00:17:50 -0500

    Added CountdownTimer module

---
+++ TimerKit/CountdownTimer.swift
@@ -0,0 +1,35 @@
+//
+//  CountdownTimer.swift
+//  TimerKit
+//
+//  Created by Simon Tsai on 11/17/16.
+//  Copyright © 2016 Simon Tsai. All rights reserved.
+//
+
+public class CountdownTimer {
+  
+  private let duration: Duration
+  
+  public init(_ duration: Duration) {
+    
+    self.duration = duration
+    
+  }
+  
+  public func start(completion: @escaping () -> Void) {
+    
+    Timer.scheduledTimer(withTimeInterval: duration.timeInterval, repeats: false, block: { _ in
+      
+      completion()
+      
+    })
+    
+  }
+  
+}
+
+public protocol Duration {
+  
+  var timeInterval: TimeInterval { get }
+  
+}
